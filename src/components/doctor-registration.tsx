9ace62783fbc4125460e71f2574c317fba32cdbd
MJ Zenith <mjzenithserv@gmail.com>
2025-08-03 16:14:52 +0000

    check the public folder you wll see my logo image use it as my logo
    it

--- src/components/doctor-registration.tsx
+++ src/components/doctor-registration.tsx
@@ -16,29 +16,6 @@ import { ChevronLeft } from "lucide-react";
 import LandingHeader from "./landing-header";
 import LandingFooter from "./landing-footer";
 
-
-const NovainHealthLogo = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg width="148" height="64" viewBox="0 0 148 64" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-        <path d="M30.4105 17.584V1.36963H44.0155V39.4628H30.4105V22.2541C30.4105 19.8932 30.4105 17.5324 30.4105 15.0883C30.322 13.1259 29.9329 11.8311 28.532 10.9634C27.0425 10.0957 24.9922 10.0957 23.0827 10.8801L12.553 15.0094V4.07227L30.4105 17.584Z" fill="#002244"/>
-        <path d="M0.999878 1.36963H16.8924V39.4628H0.999878V1.36963Z" fill="#002244"/>
-        <path d="M22.083 39.4628V24.0858L11.6669 30.4326V39.4628H22.083Z" fill="#002244"/>
-        <path d="M22.083 1.36963H11.6669V20.913L22.083 16.0826V1.36963Z" fill="#002244"/>
-        <path d="M69.1213 17.3164C69.1213 19.6773 67.6974 21.178 65.5971 21.178C63.4969 21.178 62.073 19.6773 62.073 17.3164C62.073 14.9556 63.4969 13.4548 65.5971 13.4548C67.6974 13.4548 69.1213 14.9556 69.1213 17.3164ZM65.5971 39.9237C56.6896 39.9237 49.0908 30.5694 49.0908 17.3164C49.0908 3.98016 56.6896 1.36963 65.5971 1.36963C74.4754 1.36963 82.0349 3.98016 82.0349 17.3164C82.0349 30.5694 74.4754 39.9237 65.5971 39.9237Z" fill="#002244"/>
-        <path d="M104.926 17.584V1.36963H118.531V39.4628H104.926V22.2541C104.926 19.8932 104.926 17.5324 104.926 15.0883C104.837 13.1259 104.448 11.8311 103.047 10.9634C101.558 10.0957 99.5076 10.0957 97.5981 10.8801L87.0684 15.0094V4.07227L104.926 17.584Z" fill="#002244"/>
-        <path d="M84.9843 1.36963H100.877V39.4628H84.9843V1.36963Z" fill="#002244"/>
-        <path d="M124.405 1.36963H140.298V39.4628H124.405V1.36963Z" fill="#002244"/>
-        <path d="M141.492 1.36963H157.942L149.336 17.3164L157.73 39.4628H141.636L136.96 27.5372H131.544V39.4628H118.531V1.36963H136.721C138.63 1.36963 140.384 2.58105 141.002 4.41907C141.62 6.25708 141.62 8.24158 141.002 10.0796C140.384 11.9176 138.63 13.0132 136.721 13.0132H131.544V18.6675H135.702L141.492 1.36963Z" fill="#46C8F5"/>
-        <path d="M78.3278 44.1543L157.11 44.1543" stroke="#46C8F5" strokeWidth="2.5" strokeLinecap="round"/>
-        <path d="M99.3093 44.1548L78.3279 63.9375" stroke="#46C8F5" strokeWidth="2.5" strokeLinecap="round"/>
-        <path d="M116.818 44.1548L137.799 63.9375" stroke="#46C8F5" strokeWidth="2.5" strokeLinecap="round"/>
-        <path d="M121.155 57.391C121.155 51.528 125.751 46.9316 131.614 46.9316H133.626C139.489 46.9316 144.084 51.528 144.084 57.391V63.9375H121.155V57.391Z" stroke="#46C8F5" strokeWidth="2.5" strokeLinecap="round"/>
-        <path d="M151.722 46.9316H148.117V63.9375H151.722V46.9316Z" stroke="#46C8F5" strokeWidth="2.5" strokeLinecap="round"/>
-        <path d="M162.067 63.9375L152.88 46.9316" stroke="#46C8F5" strokeWidth="2.5" strokeLinecap="round"/>
-        <path d="M153.541 63.3853L164.71 46.9316" stroke="#46C8F5" strokeWidth="2.5" strokeLinecap="round"/>
-    </svg>
-)
-
-
 export default function DoctorRegistration() {
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
@@ -63,7 +40,7 @@ export default function DoctorRegistration() {
                     </Link>
                 </div>
               <div className="flex justify-center mb-6">
-                <NovainHealthLogo />
+                <Image src="/logo.png" alt="NovainHealth Logo" width={148} height={64} />
               </div>
               
               <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center mb-6 gap-2">
--- src/components/patient-login.tsx
+++ src/components/patient-login.tsx
@@ -7,45 +7,6 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-const NovainLogo = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg {...props} width="124" height="34" viewBox="0 0 124 34" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M25.8079 9.30872V1.02734H33.0116V22.613H25.8079V14.1201C25.8079 13.091 25.8079 12.062 25.8079 10.9907C25.7656 10.1508 25.5562 9.56208 24.8827 9.17721C24.2092 8.79234 23.3241 8.79234 22.4812 9.135L17.6713 10.8142V2.8858L25.8079 9.30872Z" fill="#002244"/>
-        <path d="M0.852051 1.02734H8.05574V22.613H0.852051V1.02734Z" fill="#002244"/>
-        <path d="M17.0689 22.613V14.9961L9.65361 17.792V22.613H17.0689Z" fill="#002244"/>
-        <path d="M17.0689 1.02734H9.65361V13.6264L17.0689 11.2371V1.02734Z" fill="#002244"/>
-        <path d="M51.841 11.8949C51.841 12.9239 51.1528 13.5915 50.1534 13.5915C49.154 13.5915 48.4658 12.9239 48.4658 11.8949C48.4658 10.8658 49.154 10.1982 50.1534 10.1982C51.1528 10.1982 51.841 10.8658 51.841 11.8949ZM50.1534 22.8245C45.3435 22.8245 41.5173 18.7844 41.5173 11.8949C41.5173 4.96299 45.3435 1.02734 50.1534 1.02734C54.9633 1.02734 58.7472 4.96299 58.7472 11.8949C58.7472 18.7844 54.9633 22.8245 50.1534 22.8245Z" fill="#002244"/>
-        <path d="M78.6942 9.30872V1.02734H85.8979V22.613H78.6942V14.1201C78.6942 13.091 78.6942 12.062 78.6942 10.9907C78.6519 10.1508 78.4425 9.56208 77.769 9.17721C77.0955 8.79234 76.2104 8.79234 75.3675 9.135L70.5576 10.8142V2.8858L78.6942 9.30872Z" fill="#002244"/>
-        <path d="M63.7383 1.02734H70.942V22.613H63.7383V1.02734Z" fill="#002244"/>
-        <path d="M93.3039 1.02734H100.508V22.613H93.3039V1.02734Z" fill="#002244"/>
-        <path d="M101.455 1.02734H110.384L105.107 11.8949L110.173 22.613H101.58L98.636 16.3421H93.1345V22.613H85.9308V1.02734H98.4665C99.4659 1.02734 100.351 1.54188 100.815 2.43962C101.279 3.33736 101.279 4.40871 100.815 5.30645C100.351 6.20419 99.4659 6.71873 98.4665 6.71873H93.1345V10.1135H97.7497L101.455 1.02734Z" fill="#46C8F5"/>
-        <path d="M40.2458 31.5208L34.1171 25.8624L40.2458 31.5208Z" fill="#46C8F5"/>
-        <path d="M40.542 31.2417L34.1171 25.2842L40.542 31.2417Z" fill="#46C8F5"/>
-        <path d="M34.1171 25.2842L40.5843 31.2839L34.1171 25.2842Z" fill="#46C8F5"/>
-        <path d="M40.5843 31.2839L34.1171 25.2842L40.5843 31.2839Z" fill="#46C8F5"/>
-        <path d="M34.1171 25.2842L40.542 31.2417L34.1171 25.2842Z" fill="#46C8F5"/>
-        <path d="M40.542 31.2417L34.1171 25.2842L40.542 31.2417Z" fill="#46C8F5"/>
-        <path d="M34.1171 25.2842L40.2458 31.5208L34.1171 25.2842Z" fill="#46C8F5"/>
-        <path d="M40.2458 31.5208L34.1171 25.2842L40.2458 31.5208Z" fill="#46C8F5"/>
-        <path d="M40.2035 25.0938H33.8208L40.2035 25.0938Z" fill="#46C8F5"/>
-        <path d="M40.2035 25.0938H33.8208" stroke="#46C8F5" strokeWidth="1.5" strokeLinecap="round"/>
-        <path d="M45.5426 25.0938H41.6885" stroke="#46C8F5" strokeWidth="1.5" strokeLinecap="round"/>
-        <path d="M45.416 31.5208L40.2458 25.2842" stroke="#46C8F5" strokeWidth="1.5" strokeLinecap="round"/>
-        <path d="M40.542 31.2417L34.1171 25.2842" stroke="#46C8F5" strokeWidth="1.5" strokeLinecap="round"/>
-        <path d="M49.4912 31.5208H55.8739" stroke="#46C8F5" strokeWidth="1.5" strokeLinecap="round"/>
-        <path d="M52.6826 31.5208V25.2419" stroke="#46C8F5" strokeWidth="1.5" strokeLinecap="round"/>
-        <path d="M59.3204 31.5208H65.7031" stroke="#46C8F5" strokeWidth="1.5" strokeLinecap="round"/>
-        <path d="M62.5118 31.5208V25.2419" stroke="#46C8F5" strokeWidth="1.5" strokeLinecap="round"/>
-        <path d="M69.0494 28.3813C69.0494 26.5925 70.472 25.2419 72.1866 25.2419H73.0336C74.7482 25.2419 76.1708 26.5925 76.1708 28.3813V31.5208H69.0494V28.3813Z" stroke="#46C8F5" strokeWidth="1.5" strokeLinecap="round"/>
-        <path d="M80.0197 25.2419H78.291V31.5208H80.0197V25.2419Z" stroke="#46C8F5" strokeWidth="1.5" strokeLinecap="round"/>
-        <path d="M86.317 31.5208L81.1467 25.2842" stroke="#46C8F5" strokeWidth="1.5" strokeLinecap="round"/>
-        <path d="M81.4258 31.2417L87.8507 25.2842" stroke="#46C8F5" strokeWidth="1.5" strokeLinecap="round"/>
-        <path d="M90.3113 25.2419V31.5208" stroke="#46C8F5" strokeWidth="1.5" strokeLinecap="round"/>
-        <path d="M90.3113 27.5029H93.3039" stroke="#46C8F5" strokeWidth="1.5" strokeLinecap="round"/>
-        <path d="M95.6698 25.2419L99.6384 31.5208L103.607 25.2419" stroke="#46C8F5" strokeWidth="1.5" strokeLinecap="round"/>
-        <path d="M123.148 11.8949C123.148 12.9239 122.46 13.5915 121.461 13.5915C120.461 13.5915 119.773 12.9239 119.773 11.8949C119.773 10.8658 120.461 10.1982 121.461 10.1982C122.46 10.1982 123.148 10.8658 123.148 11.8949ZM121.461 22.8245C116.651 22.8245 112.825 18.7844 112.825 11.8949C112.825 4.96299 116.651 1.02734 121.461 1.02734C126.271 1.02734 130.055 4.96299 130.055 11.8949C130.055 18.7844 126.271 22.8245 121.461 22.8245Z" fill="#002244"/>
-    </svg>
-);
-
 const GoogleIcon = (props: React.SVGProps<SVGSVGElement>) => (
     <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
         <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"/>
@@ -70,7 +31,7 @@ export default function PatientLogin() {
       <div className="w-full lg:w-1/2 flex flex-col items-center justify-center p-8 overflow-y-auto">
         <div className="w-full max-w-md">
           <div className="flex justify-center mb-8">
-            <NovainLogo />
+            <Image src="/logo.png" alt="NovainHealth Logo" width={124} height={34} />
           </div>
           <h1 className="text-3xl font-bold text-center mb-2">Login</h1>
           <p className="text-center text-muted-foreground mb-6">Hello, welcome back!</p>
