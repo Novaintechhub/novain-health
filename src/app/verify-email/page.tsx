e0aeff602ec0a531968729f13e8f7c61d11c494a
MJ Zenith <mjzenithserv@gmail.com>
2025-08-05 23:46:10 +0000

    758a8473beda: Verifying Checksum 758a8473beda: Download complete 4176336

--- src/app/verify-email/page.tsx
+++ src/app/verify-email/page.tsx
@@ -1,5 +1,15 @@
 import OtpVerification from "@/components/otp-verification";
+import { Suspense } from "react";
+
+// A component that uses useSearchParams must be wrapped in a Suspense boundary.
+const OtpVerificationComponent = () => {
+  return <OtpVerification />;
+};
 
 export default function VerifyEmailPage() {
-  return <OtpVerification />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <OtpVerificationComponent />
+    </Suspense>
+  );
 }
